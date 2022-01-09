014f67d9f3f46b591f616983db7830c91081ac3e
DESKTOP-9U662K2\Mushfiqur <rahmanmushfiq94@gmail.com>
2022-01-10 00:56:54 +0600

    login error message updated

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -87,7 +87,7 @@ class RegisterController extends BaseController
 
             return $this->sendResponse($success, 'User login successfully.');
         } else {
-            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+            return $this->sendError('Wrong username or password', ['error' => 'Unauthorised']);
         }
     }
 
